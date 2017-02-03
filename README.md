9907c9cd49aa7e4b9d029026dab426cca8761097
Christian Heyn <chris.heyn.dd@gmail.com>
2017-02-04 00:13:05 +0100

    init repo

---
+++ README.md
@@ -0,0 +1,30 @@
+# on-ios-resize
+**`window.oniosresize`**
+
+Fires event everytime **after the window _height_ resized**, also on show or hide ios-adress-bar on mobile safari/chrome.
+
+## Install
+```sh
+$ npm install on-ios-resize --save
+```
+
+
+## Usage
+```js
+require('on-ios-resize');
+
+window.oniosresize = function (direction) {
+	console.log(direction);
+};
+```
+
+Parameter `direction` is `1` if window height becomes bigger, `-1` if window height becomes smaller.
+
+
+#### Tested?
+If you tested this package and it dosn't worked for you, please create an issue [here](https://github.com/christianheyn/on-ios-resize/issues).
+
+___
+
+**Author**
+[Christian Heyn](https://github.com/christianheyn)
---
+++ index.js
@@ -0,0 +1,73 @@
+(function (factory) {
+    if (typeof exports === 'object') {
+        // npm
+        module.exports = factory;
+    }
+}(function () {
+	var currentHeight = 0,
+		timeout,
+		callbackCollection = [];
+
+	if (typeof window.oniosresize !== 'undefined') {
+		return false;
+	}
+
+	Object.defineProperty(window,'oniosresize',{
+	    set: function(callback){
+	        if (typeof callback === 'function') {
+	        	callbackCollection.push(callback);
+	        }
+	    },
+
+	    get: function() {
+	    	return 'Use window.oniosresize = function() {/* ... */};';
+	    }
+	});
+
+	var tester = document.createElement('div'),
+		checkResize = function () {
+			var heightNow = tester.offsetHeight;
+
+			// is bigger
+			if (heightNow > currentHeight) {
+				currentHeight = heightNow;
+				return 1;
+			}
+
+			// is smaller
+			if (heightNow < currentHeight) {
+				currentHeight = heightNow;
+				return -1;
+			}
+
+			return false;
+		};
+
+	tester.setAttribute('style', [
+		'position:fixed;',
+		'display: block;',
+		'top: 0;',
+		'bottom: 0;',
+	].join(''));
+
+	document.body.appendChild(tester);
+	currentHeight = tester.offsetHeight;
+
+	window.onscroll = function () {
+		window.clearTimeout(timeout);
+		timeout = window.setTimeout(function () {
+			var i = 0,
+				result = checkResize();
+
+			if (typeof result === 'number') {
+				for (i; i < callbackCollection.length; i += 1) {
+					if ( typeof callbackCollection[i] === 'function') {
+						callbackCollection[i](result);
+					}
+				}
+			}
+		}, 100);
+	};
+
+	return window.oniosresize;
+}()));
